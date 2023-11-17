37a87855460b9b7cbb3c182b77e79e35f9e3b786
Isac Lopes <sisaclp15@gmail.com>
2023-11-17 18:49:36 -0300

    create predict pipeline

--- src/pokemon_project/pipeline_registry.py
+++ src/pokemon_project/pipeline_registry.py
@@ -18,14 +18,17 @@ def register_pipelines() -> dict[str, Pipeline]:
     from pokemon_project.pipelines import pre_processing as pp
     from pokemon_project.pipelines import data_engineering as de
     from pokemon_project.pipelines import data_science as ds
+    from pokemon_project.pipelines import predict as pd
+
     
     pre_processing_pipeline = pp.create_pipeline()
     data_engineering = de.create_pipeline()
     data_science = ds.create_pipeline()
-
+    predict = pd.create_pipeline()
     return {'pp': pre_processing_pipeline,
             'de': data_engineering,
             'ds': data_science,
-             "__default__":(pre_processing_pipeline + data_engineering + data_science)
+            'pred': predict,
+            "__default__":(pre_processing_pipeline + data_engineering + data_science + predict)
             
             }
